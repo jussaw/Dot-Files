19ab3c406769275c69963b43cf0cdb804e9504d0
Justin Sawyer <jtsaw36@gmail.com>
2019-09-30 20:28:31 -0400

    reorganizes repo and adds fish config

---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -lah'
+alias c='clear'
+alias cl='clear; and ls -lah'
+alias ..='cd ..'
+alias cl='clear; and ls -lah'
+alias v='vim'
+alias update='sudo apt update; and sudo apt upgrade -y'
+alias home='ssh pi@jussaw.duckdns.org'
+alias pi3='ssh pi@192.168.1.20'
+alias pi4='ssh pi@192.168.1.21'
+
+
+function fish_greeting
+	cat /etc/motd
+end
+set fish_greeting
+
